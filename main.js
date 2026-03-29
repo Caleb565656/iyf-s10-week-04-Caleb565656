19560415ba990ff70a50279bcb5417a7c50527ad
Caleb565656 <saiunicaleb@gmail.com>
2026-03-30 00:28:47 +0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,14 @@
+// Declare variables
+let name = "Caleb Saiuni"; // your name
+let age = 18; // your age
+let isStudent = true; // boolean
+let favoriteColors = ["black", "blue", "red"]; // array
+let todayDate = new Date(); // current date
+
+// Log them to the console
+console.log("My name is: " + name);
+console.log("I am " + age + " years old");
+console.log("Am I a student? " + isStudent);
+console.log("My favorite colors are: " + favoriteColors.join(", "));
+console.log("Today's date is: " + todayDate);
+
